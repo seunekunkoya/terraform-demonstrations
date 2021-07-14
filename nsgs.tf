447567ee4d2977b4dabdb1908d29b5c77efaff11
ekunkoya <ekunkoya.seun@gmail.com>
2021-07-14 16:18:42 +0000

    changed feature

--- nsgs.tf
+++ nsgs.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "nsgs" {
    name         = "NSGs"
    location     = "${var.loc}"
